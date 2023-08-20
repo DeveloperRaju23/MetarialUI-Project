9e6c760a5de03c50393ee748777a401c18bca41f
Developerraju23 <rajukumaraju.rk@gmail.com>
2023-08-20 22:55:51 +0600

    first metarial ui projects

---
+++ src/Shared/Header.jsx
@@ -0,0 +1,16 @@
+import { AppBar, Container, Toolbar } from '@mui/material'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <AppBar >
+        <Container>
+        <Toolbar>
+        Header
+        </Toolbar>
+        </Container>
+        </AppBar >
+  )
+}
+
+export default Header
